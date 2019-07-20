74c1c6e4f7ea920eeddc14e1ef8f89f4a9d1cd04
zhu leung <2809728252@qq.com>
2019-07-20 14:07:14 +0800

    add thermal controller on the bmi088

--- Src/bmi_interface.h
+++ Src/bmi_interface.h
@@ -4,6 +4,9 @@
 #include  "icm_20602.h"
 #include "ahrs.h"
 
+#define update_period 1280
+#define stable_temperature 48
+
 float get_temperature(void);
 
 #endif
--- Src/thermal_control.c
+++ Src/thermal_control.c
@@ -6,8 +6,19 @@ BaseType_t result = pdFALSE;
 TaskHandle_t thermal_crtl_h;
 
 float temperature_f = 25.0;
+float thermal_Kp = 3, thermal_Ki = 0.2;
 
 void thermal_ctrl_task(void *p);
+
+
+void limit_int(int *x , int max, int min)
+{
+  if(*x > max)
+    *x = max;
+  if(*x < min)
+    *x = min;
+}
+
 
 void thermal_ctrl_init(void)
 {
@@ -24,12 +35,26 @@ void thermal_ctrl_init(void)
   }
 }
 
+
 void thermal_ctrl_task(void *p)
 {
-#define update_period 1250
+  float Intergral = 0, err, out;
+  int pwm = 200;
   for(;;)
   {
     temperature_f = get_temperature();
+    err = temperature_f - stable_temperature;
+    out = err * thermal_Kp;
+    /*Intergral += err;
+    if(Intergral > 50)
+      Intergral = 50;
+    if(Intergral < -50)
+      Intergral = -50;
+    out += Intergral;*/
+    pwm -= out;
+    limit_int( &pwm, 999, 0);
+    TIM4->CCR3 = pwm;
     vTaskDelay(update_period);
   }
 }
+
